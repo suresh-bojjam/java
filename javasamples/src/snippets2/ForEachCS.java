b17ae012ee6225e795f7d2f8b57cf02b773fcc91
suresh <bojjamsuresh@gmail.com>
2016-03-11 12:13:35 +0530

    ForEach Control Structure

---
+++ javasamples/src/snippets2/ForEachCS.java
@@ -0,0 +1,13 @@
+package snippets2;
+
+/**
+ * Created by root on 11/3/16.
+ */
+public class ForEachCS {
+    public static void main(String args[]){
+        int num[]={1,2,3,4,5,6,7,8,9};
+        for(int x:num){
+            System.out.println(x);
+        }
+    }
+}
