7add4c18d964843778c98d6868d52a1d4bd6e53c
suresh <bojjamsuresh@gmail.com>
2016-03-11 11:13:37 +0530

    Reversing a Number

--- javasamples/src/DoWhileCS.java
+++ javasamples/src/DoWhileCS.java
@@ -7,11 +7,11 @@ public class DoWhileCS {
         int val2=val;
         int reminder;
         int rev=0;
-        while(val2>0){
+        do{
             reminder=val2%10;
             val2=val2/10;
             rev=rev*10+reminder;
-        }
+        }while(val2>0);
         System.out.println("rev: "+rev);
     }
 }
