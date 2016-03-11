b943eea6b53d6758145ee707048629bb9f90f0f7
suresh <bojjamsuresh@gmail.com>
2016-03-11 11:18:36 +0530

    Switch Case Control Structure

---
+++ javasamples/src/SwitchCaseCS.java
@@ -0,0 +1,19 @@
+/**
+ * Created by root on 11/3/16.
+ */
+/*Switch case Control Structure*/
+public class SwitchCaseCS {
+    public static void main(String args[]){
+        int trigger=Integer.parseInt(args[0]);
+        switch(trigger){
+            case 1:
+                    System.out.println("One");
+                    break;
+            case 2: System.out.println("Two");
+                    break;
+            default:
+                    System.out.println("Running Default Case, gonna Exit");
+                    break;
+        }
+    }
+}
