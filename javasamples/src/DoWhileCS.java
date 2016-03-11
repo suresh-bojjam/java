b0215e30af7f64d9b7ba1d92f74ba1e14a68ce63
suresh <bojjamsuresh@gmail.com>
2016-03-11 11:10:12 +0530

    Do While Control Structure, Reversing a Number

---
+++ javasamples/src/DoWhileCS.java
@@ -0,0 +1,17 @@
+/**
+ * Created by root on 11/3/16.
+ */
+public class DoWhileCS {
+    public static void main(String args[]){
+        int val=12345;
+        int val2=val;
+        int reminder;
+        int rev=0;
+        while(val2>0){
+            reminder=val2%10;
+            val2=val2/10;
+            rev=rev*10+reminder;
+        }
+        System.out.println("rev: "+rev);
+    }
+}
