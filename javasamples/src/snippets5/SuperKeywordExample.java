601f91b0eba0377842f8d4f1dbc2b10f1537e072
suresh <bojjamsuresh@gmail.com>
2016-03-13 13:46:30 +0530

    super keyword test

---
+++ javasamples/src/snippets5/SuperKeywordExample.java
@@ -0,0 +1,13 @@
+package snippets5;
+
+/**
+ * Created by root on 13/3/16.
+ */
+public class SuperKeywordExample {
+    public static void main(String[] args)
+    {
+        SB obj=new SB(2,3);
+        obj.display();
+
+    }
+}
