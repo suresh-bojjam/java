0a1f343a6b65a130c8e587ccfcaa3cdbe2f61f3a
suresh <bojjamsuresh@gmail.com>
2016-03-13 12:47:29 +0530

    Interface Example

---
+++ javasamples/src/snippets4/Compute.java
@@ -0,0 +1,14 @@
+package snippets4;
+
+/**
+ * Created by root on 13/3/16.
+ */
+public interface Compute {
+    public int sun(int a,int b);
+    public int multiplication(int a,int b);
+    public int division(int a,int b);
+    public int subtraction(int a,int b);
+    public int square(int a);
+    public int squareRoot(int a);
+    public int cube(int a);
+}
